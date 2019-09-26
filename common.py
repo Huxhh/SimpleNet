bcc3d81592760901d1f3aa89ed8d4db93d25e564
扈晓晖 <563255387@qq.com>
2019-09-26 11:12:44 +0800

    add layer

--- common.py
+++ common.py
@@ -33,3 +33,20 @@ def cross_entropy_error(y, t):
     batch_size = y.shape[0]
     return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
+
+def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
+    N, C, H, W = input_data.size
+    out_h = (H + pad * 2 - filter_h) // stride + 1
+    out_w = (W + pad * 2 - filter_w) // stride + 1
+
+    img = np.pad(input_data, [(0, 0), (0, 0), (pad, pad), (pad, pad)], 'constant')
+    col = np.zero(N, C, filter_h, filter_w, out_h, out_w)
+
+    for y in range(filter_h):
+        y_max = y + stride * out_h
+        for x in range(filter_w):
+            x_max = x + stride * out_w
+            col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
+
+    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N * out_h * out_w, -1)
+    return col
